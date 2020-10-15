c649a3e924d44518863d0d63a4633a92921b5a5d
jmarinprofe <jmarinprofe@gmail.com>
2020-10-15 15:45:38 +0200

    Commit

--- RomanCalc/RomanCalc/Program.cs
+++ RomanCalc/RomanCalc/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
-
+﻿
+// Este es el controlador principal
 namespace RomanCalc
 {
     class Program
     {
+        public static void LaunchMainMenu()
+        {
+            while (true)
+            {
+                System.Console.Clear();
+                UserInterface.PrintMainMenu();
+                string option = System.Console.ReadLine();
+                System.Console.WriteLine("La opción introducida es " + option);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LaunchMainMenu();
         }
     }
 }
---
+++ RomanCalc/RomanCalc/UserInterface.cs
@@ -0,0 +1,15 @@
+﻿namespace RomanCalc
+{
+    class UserInterface
+    {
+        public static void PrintMainMenu()
+        {
+            System.Console.WriteLine("     CALCULADORA ROMÁN");
+            System.Console.WriteLine("------------------------------");
+            System.Console.WriteLine("1) Sumar una serie de números.");
+            System.Console.WriteLine("2) Restar una serie de números.");
+            System.Console.WriteLine("...");
+
+        }
+    }
+}
